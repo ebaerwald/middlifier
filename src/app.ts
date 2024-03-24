823ab66f6d03adedbc3968014dedf25f72ff80e3
ebaerwald <baerwald.erik@gmail.com>
2024-03-24 11:51:07 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -5,26 +5,6 @@ import fs from 'fs';
 export function buildApp(config: MidConfig)
 {
     const appPath = config.paths?.server ?? './app';
-    setupNode([
-        "typescript",
-        "express"
-    ], appPath);
+    createDirIfNotExistent(appPath);
     process.chdir(appPath);
-    // nodemon.json
-    fs.writeFileSync('nodemon.json', arrayToString([
-        '{',
-        '   "ext": "ts",',
-        '   "ignore": [".git", "node_modules/**/node_modules", "src/**/*.spec.ts"],',
-        '   "execMap": {',
-        '       "ts": "node --require ts-node/register"',
-        '   "},',
-        '   "watch": ["src/"]',
-        '}'
-    ]));
-    // ---------------------------------------
-    fs.writeFileSync('tsconfig.json', arrayToString([
-        
-    ]));
-    createDirIfNotExistent('./src');
-    process.chdir('./src')
 }
--- src/index.ts
+++ src/index.ts
@@ -81,6 +81,34 @@ export function init()
         '',
         'start(config);',
     ]));
+    // nodemon.json
+    fs.writeFileSync('nodemon.json', arrayToString([
+        '{',
+        '   "ext": "ts",',
+        '   "ignore": [".git", "node_modules/**/node_modules", "src/**/*.spec.ts"],',
+        '   "execMap": {',
+        '       "ts": "node --require ts-node/register"',
+        '   "},',
+        '   "watch": ["src/"]',
+        '}'
+    ]));
+    // ---------------------------------------
+    // tsconfig.json
+    execSync("npx tsc --init");
+    let tsconfigContent = fs.readFileSync('tsconfig.json', { encoding: 'utf-8', flag: 'r' });
+    tsconfigContent = tsconfigContent.replace('// "outDir": "./"', '"outdir": "./dist');
+    fs.writeFileSync('tsconfig.json', tsconfigContent);
+    // ---------------------------------------
+    // package.json
+    let packageJsonContent = fs.readFileSync('package.json', { encoding: 'utf-8', flag: 'r' });
+    let packageJson = JSON.parse(packageJsonContent);
+    if (!packageJson.scripts) packageJson.scripts = {};
+    if (!packageJson.scripts.build) packageJson.scripts.build = "npx tsc";
+    if (!packageJson.scripts.dev) packageJson.scripts.dev = "nodemon src/index.ts"
+    if (!packageJson.scripts.start) packageJson.scripts.start = "node dist/index.js";
+    packageJsonContent = JSON.stringify(packageJsonContent);
+    fs.writeFileSync('package.json', packageJsonContent);
+    // ---------------------------------------
     process.chdir('..');
 }
 
